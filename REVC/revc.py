989f34cca6409dad180b6f88595ed9944c9866ac
NicholasDeLateur <Nicholas.DeLateur@gmail.com>
2020-03-17 13:21:23 -0700

    list comprehension for revc

--- REVC/revc.py
+++ REVC/revc.py
@@ -5,11 +5,10 @@ Created on Tue Mar 17 12:58:20 2020
 @author: Revel
 """
 complements = {'C':'G', 'G':'C', 'A':'T', 'T':'A'}
+
 with open('rosalind_revc.txt', 'r+') as file:
     dna_string = file.read().strip('\n')
 
-rc = ''  
-for bp in dna_string[::-1]:
-    rc += complements[bp]
+rc = ''.join([complements[bp] for bp in reversed(dna_string)])
 
 print(rc)
